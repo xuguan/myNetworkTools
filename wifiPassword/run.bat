25f969e6d1f69435dda1858009e2a34883d752bd
xuguan <guanxu86@gmail.com>
2016-07-22 13:31:36 +0800

    添加wifi密码查询功能
    显示本地记录的wifi密码

---
+++ wifiPassword/run.bat
@@ -0,0 +1,11 @@
+﻿@echo off
+
+for /f "skip=9 tokens=1,2 delims=:" %%i in ('netsh wlan show profiles') do (
+
+@echo SSID %%j
+REM @echo %%j | findstr -i -v echo | netsh wlan show profiles %%j key=clear|findstr /i "关键内容 安全密钥"|findstr /v /c:"SSID 名称"
+
+netsh wlan show profiles %%j key=clear |findstr /c:"安全密钥               : 不存在"
+@echo %%j | findstr -i -v echo | netsh wlan show profiles %%j key=clear|findstr /i "关键内容"
+)
+@pause
